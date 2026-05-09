e8d2374109df70a4d64747d08b887cd6f987919b
liarr23 <xzx070323@gmail.com>
2026-05-09 21:33:09 +0800

    commit

--- src/ui/UIManager.h
+++ src/ui/UIManager.h
@@ -4,6 +4,7 @@
 class UIManager {
 public:
     UIManager(int score = 0, int highScore = 0, int lives = 3);
+    ~UIManager();
     void render(sf::RenderWindow& window);
 private:
     int m_score;
